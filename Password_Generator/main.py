4894f7ca9c51e7e90c027f9fb5a28616ea817c08
Nee1608 <neelampatar65@gmail.com>
2023-10-19 18:48:19 +0530

    updated

--- Password_Generator/main.py
+++ Password_Generator/main.py
@@ -5,18 +5,18 @@ def pass_gen(lenght):
     list_of_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_"
     
     #creating a empty password string
-    passwd = " "
+    password = " "
 
     #loop that will go upto the lenght of password
     for i in range(lenght):
         #random.choice() function is used to select random character from the list_of_chars and concatenate it with passwd string
-        passwd = passwd + random.choice(list_of_chars)
+        password = password + random.choice(list_of_chars)
 
-    return passwd
+    return password
 
 
 print("Enter the lenght of password to be generated:  ")
 lenght = int(input())
 
-passwd = pass_gen(lenght)
-print("Random generated password is:  ",passwd)
+password = pass_gen(lenght)
+print("Random generated password is:  ",password)
